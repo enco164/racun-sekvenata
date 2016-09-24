f9fe62e0a6faffdd05fc2e1bba2d30a3dc5332e5
milan <milanzp@hotmail.com>
2016-09-24 19:06:08 +0200

    weakeing i rename left/right umesto ant...wtf/consequence

--- SequentCalculus/Sequent.ts
+++ SequentCalculus/Sequent.ts
@@ -8,175 +8,195 @@ import {Imp} from "../FormulaTree/Imp";
 import {Not} from "../FormulaTree/Not";
 
 export class Sequent {
-    private _antecedents: Formula[];
-    private _consequents: Formula[];
+    private _left: Formula[];
+    private _right: Formula[];
 
-    constructor(antecedents: Formula[], consequents: Formula[]){
-        this._antecedents = [];
-        if (antecedents) {
-            for (let i = 0; i < antecedents.length; i++) {
-                this._antecedents.push(antecedents[i]);
+    constructor(left: Formula[], right: Formula[]){
+        this._left = [];
+        if (left) {
+            for (let i = 0; i < left.length; i++) {
+                this._left.push(left[i]);
             }
         }
 
-        this._consequents = [];
-        if (consequents) {
-            for (let i = 0; i < consequents.length; i++) {
-                this._consequents.push(consequents[i]);
+        this._right = [];
+        if (right) {
+            for (let i = 0; i < right.length; i++) {
+                this._right.push(right[i]);
             }
         }
     }
 
     leftConjunction(idx: number): Sequent[] {
-        let c: And = (<And>this._antecedents[idx]);
-        let cloneObj = new (<any>this).constructor(this._antecedents, this._consequents);
-        cloneObj.antecedents.push(c.operand1, c.operand2);
-        cloneObj.antecedents.splice(idx, 1);
+        let c: And = (<And>this._left[idx]);
+        let cloneObj = new (<any>this).constructor(this._left, this._right);
+        cloneObj.left.push(c.operand1, c.operand2);
+        cloneObj.left.splice(idx, 1);
         return [cloneObj];
     }
 
     rightConjunction(idx: number): Sequent[] {
-        let c: And = (<And>this._consequents[idx]);
-        let cloneObj1 = new (<any>this).constructor(this._antecedents, this._consequents);
-        let cloneObj2 = new (<any>this).constructor(this._antecedents, this._consequents);
+        let c: And = (<And>this._right[idx]);
+        let cloneObj1 = new (<any>this).constructor(this._left, this._right);
+        let cloneObj2 = new (<any>this).constructor(this._left, this._right);
 
-        let consequents1 = cloneObj1.consequents;
-        consequents1.push(c.operand1);
-        consequents1.splice(idx, 1);
-        cloneObj1.consequents = consequents1;
+        let right1 = cloneObj1.right;
+        right1.push(c.operand1);
+        right1.splice(idx, 1);
+        cloneObj1.right = right1;
 
-        let consequents2 = cloneObj2.consequents;
-        consequents2.push(c.operand2);
-        consequents2.splice(idx, 1);
-        cloneObj2.consequents = consequents2;
+        let right2 = cloneObj2.right;
+        right2.push(c.operand2);
+        right2.splice(idx, 1);
+        cloneObj2.right = right2;
 
         return [cloneObj1, cloneObj2];
     }
 
     leftDisjunction(idx: number): Sequent[] {
-        let d: Or = (<Or>this._antecedents[idx]);
-        let cloneObj1 = new (<any>this).constructor(this._antecedents, this._consequents);
-        let cloneObj2 = new (<any>this).constructor(this._antecedents, this._consequents);
+        let d: Or = (<Or>this._left[idx]);
+        let cloneObj1 = new (<any>this).constructor(this._left, this._right);
+        let cloneObj2 = new (<any>this).constructor(this._left, this._right);
 
-        let antecedents1 = cloneObj1.antecedents;
-        antecedents1.push(d.operand1);
-        antecedents1.splice(idx, 1);
-        cloneObj1.antecedents = antecedents1;
+        let left1 = cloneObj1.left;
+        left1.push(d.operand1);
+        left1.splice(idx, 1);
+        cloneObj1.left = left1;
 
-        let antecedents2 = cloneObj2.antecedents;
-        antecedents2.push(d.operand2);
-        antecedents2.splice(idx, 1);
-        cloneObj2.antecedents = antecedents2;
+        let left2 = cloneObj2.left;
+        left2.push(d.operand2);
+        left2.splice(idx, 1);
+        cloneObj2.left = left2;
 
         return [cloneObj1, cloneObj2];
     }
 
     rightDisjunction(idx: number): Sequent[] {
-        let d: Or = (<Or>this._consequents[idx]);
-        let cloneObj = new (<any>this).constructor(this._antecedents, this._consequents);
+        let d: Or = (<Or>this._right[idx]);
+        let cloneObj = new (<any>this).constructor(this._left, this._right);
 
-        let consequents = cloneObj.consequents;
-        consequents.push(d.operand1);
-        consequents.push(d.operand2);
-        consequents.splice(idx, 1);
-        cloneObj.consequents = consequents;
+        let right = cloneObj.right;
+        right.push(d.operand1);
+        right.push(d.operand2);
+        right.splice(idx, 1);
+        cloneObj.right = right;
 
         return [cloneObj];
     }
 
     leftImplication(idx: number): Sequent[] {
-        let i: Imp = (<Imp>this._antecedents[idx]);
-        let cloneObj1 = new (<any>this).constructor(this._antecedents, this._consequents);
-        let cloneObj2 = new (<any>this).constructor(this._antecedents, this._consequents);
+        let i: Imp = (<Imp>this._left[idx]);
+        let cloneObj1 = new (<any>this).constructor(this._left, this._right);
+        let cloneObj2 = new (<any>this).constructor(this._left, this._right);
 
-        let antecedents1 = cloneObj1.antecedents;
-        antecedents1.splice(idx, 1);
-        let consequents1 = cloneObj1.consequents;
-        consequents1.push(i.operand1);
-        cloneObj1.antecedents = antecedents1;
-        cloneObj1.consequents = consequents1;
+        let left1 = cloneObj1.left;
+        left1.splice(idx, 1);
+        let right1 = cloneObj1.right;
+        right1.push(i.operand1);
+        cloneObj1.left = left1;
+        cloneObj1.right = right1;
 
-        let antecedents2 = cloneObj2.antecedents;
-        antecedents2.push(i.operand2);
-        antecedents2.splice(idx, 1);
-        cloneObj2.antecedents = antecedents2;
+        let left2 = cloneObj2.left;
+        left2.push(i.operand2);
+        left2.splice(idx, 1);
+        cloneObj2.left = left2;
 
         return [cloneObj1, cloneObj2];
     }
 
     rightImplication(idx: number): Sequent[] {
-        let i: Imp = (<Imp>this._consequents[idx]);
-        let cloneObj = new (<any>this).constructor(this._antecedents, this._consequents);
+        let i: Imp = (<Imp>this._right[idx]);
+        let cloneObj = new (<any>this).constructor(this._left, this._right);
 
-        let antecedents = cloneObj.antecedents;
-        antecedents.push(i.operand1);
+        let left = cloneObj.left;
+        left.push(i.operand1);
 
-        let consequents = cloneObj.consequents;
-        consequents.push(i.operand2);
-        consequents.splice(idx, 1);
+        let right = cloneObj.right;
+        right.push(i.operand2);
+        right.splice(idx, 1);
 
-        cloneObj.antecedents = antecedents;
-        cloneObj.consequents = consequents;
+        cloneObj.left = left;
+        cloneObj.right = right;
 
         return [cloneObj];
     }
 
     leftNegation(idx: number): Sequent[] {
-        let n: Not = (<Not>this._antecedents[idx]);
-        let cloneObj = new (<any>this).constructor(this._antecedents, this._consequents);
+        let n: Not = (<Not>this._left[idx]);
+        let cloneObj = new (<any>this).constructor(this._left, this._right);
 
-        let consequents = cloneObj.consequents;
-        consequents.push(n.operand);
-        let antecedents = cloneObj.antecedents;
-        antecedents.splice(idx, 1);
-        cloneObj.antecedents = antecedents;
-        cloneObj.consequents = consequents;
+        let right = cloneObj.right;
+        right.push(n.operand);
+        let left = cloneObj.left;
+        left.splice(idx, 1);
+        cloneObj.left = left;
+        cloneObj.right = right;
 
         return [cloneObj];
     }
 
 
     rightNegation(idx: number): Sequent[] {
-        let n: Not = (<Not>this._consequents[idx]);
-        let cloneObj = new (<any>this).constructor(this._antecedents, this._consequents);
+        let n: Not = (<Not>this._right[idx]);
+        let cloneObj = new (<any>this).constructor(this._left, this._right);
 
-        let antecedents = cloneObj.antecedents;
-        antecedents.push(n.operand);
-        let consequents = cloneObj.consequents;
-        consequents.splice(idx, 1);
-        cloneObj.antecedents = antecedents;
-        cloneObj.consequents = consequents;
+        let left = cloneObj.left;
+        left.push(n.operand);
+        let right = cloneObj.right;
+        right.splice(idx, 1);
+        cloneObj.left = left;
+        cloneObj.right = right;
+
+        return [cloneObj];
+    }
+    
+    leftWeakening(idx: number): Sequent[] {
+        let cloneObj = new (<any>this.constructor(this._left, this._right));
+
+        let left = cloneObj.left;
+        left.splice(idx, 1);
+        cloneObj.left = left;
 
         return [cloneObj];
     }
 
-    get antecedents():Formula[] {
-        return this._antecedents;
+    rightWeakening(idx: number): Sequent[] {
+        let cloneObj = new (<any>this.constructor(this._left, this._right));
+
+        let right = cloneObj.right;
+        right.splice(idx, 1);
+        cloneObj.right = right;
+
+        return [cloneObj];
     }
 
-    set antecedents(value:Formula[]) {
-        this._antecedents = value;
+    get left():Formula[] {
+        return this._left;
     }
 
-    get consequents():Formula[] {
-        return this._consequents;
+    set left(value:Formula[]) {
+        this._left = value;
     }
 
-    set consequents(value:Formula[]) {
-        this._consequents = value;
+    get right():Formula[] {
+        return this._right;
+    }
+
+    set right(value:Formula[]) {
+        this._right = value;
     }
 
     public toString = ():string => {
         var s = 'Sequent{';
 
-        if (this._antecedents) {
-            s += 'antecedents: [';
-            s += this._antecedents.toString() + '],';
+        if (this._left) {
+            s += 'left: [';
+            s += this._left.toString() + '],';
         }
 
-        if (this._consequents) {
-            s+=  'consequents: [';
-                s += this._consequents.toString() + ']';
+        if (this._right) {
+            s+=  'right: [';
+                s += this._right.toString() + ']';
         }
 
         s += '}';
