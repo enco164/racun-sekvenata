9957c053502daeaa8323a8e05f5af6b5caa79c7a
enco164 <enco164@gmail.com>
2016-09-24 15:07:35 +0200

    sitne izmene

--- Parser/Parser.ts
+++ Parser/Parser.ts
@@ -376,27 +376,27 @@ var parser = (function(){
                 switch($avoiding_name_collisions) {
                     case 0:/* skip whitespace */
                         break;
-                    case 1:return 15
+                    case 1:return 15;
                         break;
-                    case 2:return 14
+                    case 2:return 14;
                         break;
-                    case 3:return 13
+                    case 3:return 13;
                         break;
-                    case 4:return 11
+                    case 4:return 11;
                         break;
-                    case 5:return 12
+                    case 5:return 12;
                         break;
-                    case 6:return 10
+                    case 6:return 10;
                         break;
-                    case 7:return 9
+                    case 7:return 9;
                         break;
-                    case 8:return 8
+                    case 8:return 8;
                         break;
-                    case 9:return 7
+                    case 9:return 7;
                         break;
-                    case 10:return 6
+                    case 10:return 6;
                         break;
-                    case 11:return 5
+                    case 11:return 5;
                         break;
                 }
             },
